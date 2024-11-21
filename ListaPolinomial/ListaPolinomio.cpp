4d748039a9bd39386805a35efa950bf850b0a42f
Linkzandro <lizandrolgui123@gmail.com>
2024-11-20 22:24:31 -0300

    padrao c++

---
+++ ListaPolinomial/ListaPolinomio.cpp
@@ -0,0 +1,1 @@
+#include "ListaPolinomio.h"
---
+++ ListaPolinomial/main.cpp
@@ -0,0 +1,27 @@
+#include "ListaPolinomio.h"
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+	ListaPolinomio lista;
+	ListaPolinomio lista2;
+	lista.Inserir(2, 2);
+	lista.Inserir(2, 8);
+	lista.Inserir(2, 3);
+	lista2.Inserir(4, 3);
+	lista2.Inserir(-2, 4);
+	//lista.display();
+	//lista2.display();
+
+	//lista*=lista2;
+	ListaPolinomio lista3 = lista * lista2;
+	//lista = lista * lista2;
+	lista3.display();
+
+
+
+
+	return 0;
+}
